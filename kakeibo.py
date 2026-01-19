9d4c35183dfe72b2597a4bd1ab53fe46d7913359
mkyt0970 <m.yuto0970@gmail.com>
2026-01-19 17:25:54 +0900

    Update kakeibo.py

--- kakeibo.py
+++ kakeibo.py
@@ -5,7 +5,7 @@ import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
 
-plt.rcParams["font.family"] = "Hiragino Sans"
+plt.rcParams["font.family"] = "DejaVu Sans"
 
 #CSV読み込み
 def load_data():
